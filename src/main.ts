b235789ee2ade433504b63057413cb32d2ee13d1
chenxixian <chenxixian@gmail.com>
2020-02-02 00:51:28 +0800

    新增SwaggerModule
    Add SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './common/validation.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+
+    const options = new DocumentBuilder()
+    .setTitle('Base App NestJs API')
+    .setDescription('基本App NestJs API 项目')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
